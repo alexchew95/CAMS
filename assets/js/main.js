37f4837c85d0e35a59a76d3193cf12ef1772744d
alex chew <alexchewice95@gmail.com>
2019-02-04 16:53:04 +0800

    done 4/2/2019

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,27 @@
 /////* Common Variable List */////
-var APIUrl = 'http://localhost/lpts-ci/api/';
-//var APIUrl = 'https://lpts-ci.herokuapp.com/api/';
+//var APIUrl = 'http://localhost/lpts-ci/api/';
+var APIUrl = "https://lpts-ci.herokuapp.com/api/";
 
 /* Common function call for most html page */
 
-$(document).ready(function () {
+$(document).ready(function() {
 	// Load Session data
 	token = sessionStorage.getItem("token");
 	role = sessionStorage.getItem("role");
 	name = sessionStorage.getItem("name");
 	userId = sessionStorage.getItem("userId");
 	email = sessionStorage.getItem("email");
-	if (role == 'AGENT' || role == 'SAGENT') {
-		$('input[type="text"],input[type="number"],input[type="checkbox"], select').prop("disabled", true);
+	if (role == "AGENT" || role == "SAGENT") {
+		$(
+			'input[type="text"],input[type="number"],input[type="checkbox"], select'
+		).prop("disabled", true);
 	}
-	function activateUser(){
-		var txt=" "
-		var r =confirm("Please confirm to activate selected user")
-		if(r== true){
-			alert("User have been activated")
+	function activateUser() {
+		var txt = " ";
+		var r = confirm("Please confirm to activate selected user");
+		if (r == true) {
+			alert("User have been activated");
 		}
-		
 	}
 
 	// Check Admin Login
@@ -29,27 +30,30 @@ $(document).ready(function () {
 		if (!token) {
 			window.location = "index.html";
 		}
-
 	}
 	window.onload = checkLogin;
 	// Load TopBar and NavBar
-	$('#topbar').load('topbar.html', function () {
+	$("#topbar").load("topbar.html", function() {
 		console.log("Topbar loaded");
-
 	});
-	$('#navbar').load('navbar.html', function () {
+	$("#navbar").load("navbar.html", function() {
 		//Add Underline to active html page. This will only work on LIVE SERVER
 		var url = document.location.href;
 		//this removes the anchor at the end, if there is one
-		url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+		url = url.substring(
+			0,
+			url.indexOf("#") == -1 ? url.length : url.indexOf("#")
+		);
 		//this removes the query after the file name, if there is one
-		url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+		url = url.substring(
+			0,
+			url.indexOf("?") == -1 ? url.length : url.indexOf("?")
+		);
 		//this removes everything before the last slash in the path
 		url = url.substring(url.lastIndexOf("/") + 1, url.length);
-		$('.nav > li > a[href="' + url + '"]').addClass('active');
+		$('.nav > li > a[href="' + url + '"]').addClass("active");
 		console.log(url);
 	});
-
 });
 
 /////* Common Function List */////
@@ -59,30 +63,33 @@ Function: To get the value in URL
 Usage:	var customerId = $_GET('customerId'); 
 */
 function $_GET(q, s) {
-	s = (s) ? s : window.location.search;
-	var re = new RegExp('&amp;' + q + '=([^&amp;]*)', 'i');
-	return (s = s.replace(/^\?/, '&amp;').match(re)) ? s = s[1] : s = '';
+	s = s ? s : window.location.search;
+	var re = new RegExp("&amp;" + q + "=([^&amp;]*)", "i");
+	return (s = s.replace(/^\?/, "&amp;").match(re)) ? (s = s[1]) : (s = "");
 }
 
 function calcInstallment() {
-	var loanApplied =parseInt($("#LoanApplied").val());
-	var tenure =parseInt($("#Tenure").val());
-	if (tenure==null || tenure=="",loanApplied==null || loanApplied==""){
+	var loanApplied = parseInt($("#LoanApplied").val());
+	var tenure = parseInt($("#Tenure").val());
+	if (
+		(tenure == null || tenure == "",
+		loanApplied == null || loanApplied == "")
+	) {
 		$("#Installment").val("-");
 	}
-	$("#Installment").val(loanApplied/tenure);
-
+	$("#Installment").val(loanApplied / tenure);
 }
 
 function roleControl() {
-	if (role == 'AGENT' || role == 'SAGENT') {
-		$('input[type="text"],input[type="number"],input[type="checkbox"], select').prop("disabled", true);
+	if (role == "AGENT" || role == "SAGENT") {
+		$(
+			'input[type="text"],input[type="number"],input[type="checkbox"], select'
+		).prop("disabled", true);
 	}
-
 }
-function buttonRoleControl(){
-	if (role == 'AGENT' || role == 'SAGENT') {
-		alert("You are not authorized to perform such task.")
+function buttonRoleControl() {
+	if (role == "AGENT" || role == "SAGENT") {
+		alert("You are not authorized to perform such task.");
 		location.reload();
 	}
 }
@@ -98,18 +105,18 @@ function closeAndReload() {
 
 /* Reset Modal textboxs value to null */
 function resetModalTextbox() {
-	$('input[type="text"]').val('');
-	$('input[type="number"]').val('');
-	$('.custom-select').val('');
+	$('input[type="text"]').val("");
+	$('input[type="number"]').val("");
+	$(".custom-select").val("");
 }
 
 /* Button Action */
-$("#btnLogout").click(function () {
+$("#btnLogout").click(function() {
 	sessionStorage.clear();
 	window.location = "index.html";
 });
 
-$("#btnCloseModal").click(function (event) {
+$("#btnCloseModal").click(function(event) {
 	event.preventDefault();
 	closeModal();
 });
