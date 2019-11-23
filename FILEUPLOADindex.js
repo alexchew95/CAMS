f4477ed2a85b28f86d481e32ab7db4d73e34531f
THONG JJ <twf961016@gmail.com>
2019-11-24 02:34:55 +0800

    FileUpload

--- FILEUPLOADindex.js
+++ FILEUPLOADindex.js
@@ -1,13 +1,13 @@
 //UPLOAD AUTHOR BIOGRAPHY FILE
-file2.addEventListener('change',function(e){
+mergedfile.addEventListener('change',function(e){
 
     for(let i=0;i<e.target.files.length;i++){
 
-        let file2=e.target.files[i];
+        let mergedfile=e.target.files[i];
 
-        let storageRef=firebase.storage().ref("article/ "+file2.name);
+        let storageRef=firebase.storage().ref("article/ "+mergedfile.name);
 
-        let task=storageRef.put(file2);
+        let task=storageRef.put(mergedfile);
 
         task.on('state_changed',function progress(snapshot){
             
@@ -28,15 +28,15 @@ file2.addEventListener('change',function(e){
 });
 
 //UPLOAD COVER LETTER
-file3.addEventListener('change',function(e){
+coverpdffile.addEventListener('change',function(e){
 
     for(let i=0;i<e.target.files.length;i++){
 
-        let file3=e.target.files[i];
+        let coverpdffile=e.target.files[i];
 
-        let storageRef=firebase.storage().ref('article/'+file3.name);
+        var storageRef=firebase.storage().ref('article/'+coverpdffile.name);
 
-        let task=storageRef.put(file3);
+        let task=storageRef.put(coverpdffile);
 
         task.on('state_changed',function progress(snapshot){
             
@@ -56,30 +56,3 @@ file3.addEventListener('change',function(e){
     }
 });
 
-filepdf.addEventListener('change',function(e){
-
-    for(let i=0;i<e.target.files.length;i++){
-
-        let filepdf=e.target.files[i]
-
-        let storageRef=firebase.storage().ref('article'+filepdf.name);
-
-        let task=storageRef.put(filepdf);
-
-        task.on('state_changed',function progress(snapshot){
-            
-            let percentage=snapshot.bytesTransferred/snapshot.totalBytes *100;
-            
-            console.log("Upload is "+percentage+"%done");
-            switch(snapshot.state){
-                case firebase.storage.TaskState.PAUSED:
-                    console.log("Upload is Paused");
-                    break;
-                case firebase.storage.TaskState.RUNNING:
-                     console.log("Upload is Running");
-                     break;
-            }
-        })
-
-    }
-});
