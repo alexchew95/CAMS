1bd8475ffc00d8d0291b9ba075a3a7311fdb5a48
unknown <jiayanteh@gmail.com>
2019-11-24 14:09:57 +0800

    editor

---
+++ FirebaseAccount.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCHXvC3POJL7AACs6IsCbzm715PozUbaRc",
+    authDomain: "miniprojectarticlemanagement.firebaseapp.com",
+    databaseURL: "https://miniprojectarticlemanagement.firebaseio.com",
+    projectId: "miniprojectarticlemanagement",
+    storageBucket: "miniprojectarticlemanagement.appspot.com",
+    messagingSenderId: "543563658048",
+    appId: "1:543563658048:web:81eaa06410120eab6080a5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
